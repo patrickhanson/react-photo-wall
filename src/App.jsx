75c434b60c6c60e8747d83ee9e1cbbcffc522e88
Patrick Hanson <patrickhanson08@gmail.com>
2018-08-06 15:21:11 -0400

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// This URL can be combined with an photo id to fetch an photo.
+
 const PHOTO_URL = "https://picsum.photos/200?photo=";
-// This URL can be used to get an array of objects that contain information
-// about various photos.
+
 const PHOTO_LIST_URL = "https://picsum.photos/list";
 
 class App extends Component {
-  // 1. Declare a state object that will be used to track an array of photos
-  state = {
-  }
 
-  // 2. Declare a life cycle method
-  // This life cycle method should:
-  //  - will be called after the component is initially rendered
-  // - will fetch an array of photos
-  // - will add that array of photos to state once received
+  state = {
+    photos: []
+  }
+  componentDidMount() {
+    fetch(PHOTO_LIST_URL)
+      .then(res => res.json())
+      .then(data => {
+      this.setState({photos: data})
+      })
+  }
+  
   render() {
     const { photos = [] } = this.state;
     return (
       <React.Fragment>
         <header>
           <h1>Photo Wall</h1>
-          <p>
-            Start by reading App.jsx and completing the numbered steps.
-            Afterward, delete this paragraph. Then, open up App.css and
-            complete the instructions there.
-          </p>
         </header>
         <div className="collage">
-            {/* We use map here because Array.prototype.map is an expression,
-              * and for loops are not. You'll learn more about this soon! 
-              */}
+          <div className="photo">
             {photos.map( photo => 
-                <img alt={/* 3. Fill me in with the photo's filename */ ""}
-                     key={/* 4. Fill me in with the photo's id */ ""}
-                     src={/* 5. Fill me in with the photo's URL */ ""}
+                <img alt={photo.filename}
+                key={photo.id}
+                src={PHOTO_URL + photo.id}
                 />
-            )}
+              )}
+          </div>
         </div>
       </React.Fragment>
     );
